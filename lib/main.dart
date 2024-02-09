2a6658d4990dc92ee9c6b8c7f826f1755e2a6d44
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-02-09 10:53:33 -0400

    new - adicionado package intl e formatado data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/transasction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -66,7 +67,7 @@ class MyHomePage extends StatelessWidget {
                         )),
                         padding: const EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$${tr.value.toStringAsFixed(2)}',
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -85,7 +86,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: const TextStyle(
                               color: Colors.grey,
                             ),
